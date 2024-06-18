ffa0128157aef5de8a6a9bb307f9c3ddbe8b4ab2
Les <adiletibraev98@gmail.com>
2024-06-18 22:35:12 +0600

    first commit

---
+++ app/src/main/java/com/example/final_project/activities/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.example.final_project.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.final_project.R;
+
+public class SplashScreenActivity extends Activity {
+    private static final int SPLASH_DISPLAY_LENGTH = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        LottieAnimationView animationView = findViewById(R.id.lottie_animation_view);
+        animationView.setAnimation(R.raw.splash);
+        animationView.playAnimation();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
---
+++ app/src/main/java/com/example/final_project/services/MusicService.java
@@ -0,0 +1,41 @@
+package com.example.final_project.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import com.example.final_project.R;
+
+public class MusicService extends Service {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.music); // Замените your_audio_file на имя вашего аудиофайла
+        mediaPlayer.setLooping(true); // Зациклить музыку
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
